d401ae1d8a925029ae0ababdb226dc6fde101956
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-03 03:09:26 -0300

    feat: add RegisterRepository implementation

--- app/src/main/java/com/example/jetpackcomposetodoapp/repository/RegisterRepositoryImpl.kt
+++ app/src/main/java/com/example/jetpackcomposetodoapp/repository/RegisterRepositoryImpl.kt
@@ -1,15 +1,15 @@
 package com.example.jetpackcomposetodoapp.repository
 
-import com.example.jetpackcomposetodoapp.data.RegisterRepository
+import com.example.jetpackcomposetodoapp.data.AuthRepository
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class RegisterRepositoryImpl @Inject constructor(
     private val firebaseAuth: FirebaseAuth
-) : RegisterRepository {
+) : AuthRepository {
 
-    override suspend fun register(email: String, password: String) : Result<String> {
+    override suspend fun authenticate(email: String, password: String) : Result<String> {
         return try {
             val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
             Result.success(result.user?.uid ?: "User is registered but UID is null")
@@ -17,4 +17,5 @@ class RegisterRepositoryImpl @Inject constructor(
             Result.failure(e)
         }
     }
+
 }
