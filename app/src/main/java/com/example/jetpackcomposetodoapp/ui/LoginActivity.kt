30b5ed4c25f8fa7adf86334cde05b57584ddd4a5
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-03 03:07:01 -0300

    feat: add LoginActivity

---
+++ app/src/main/java/com/example/jetpackcomposetodoapp/ui/LoginActivity.kt
@@ -0,0 +1,49 @@
+package com.example.jetpackcomposetodoapp.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposetodoapp.ui.ui.theme.JetpackComposeToDoAppTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Login()
+        }
+    }
+}
+
+@Composable
+fun Login() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello!",
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeToDoAppTheme {
+        Login()
+    }
+}
