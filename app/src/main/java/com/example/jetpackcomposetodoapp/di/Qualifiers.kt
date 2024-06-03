fcdd328143feb1922c424b39dac7c85de29c04f4
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-03 03:06:01 -0300

    feat: add Register And Login qualifiers

---
+++ app/src/main/java/com/example/jetpackcomposetodoapp/di/Qualifiers.kt
@@ -0,0 +1,11 @@
+package com.example.jetpackcomposetodoapp.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Register
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Login
