ea6a2f0fae7c9a05507ff0c6e6def53bac84c649
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-05 23:06:47 -0300

    feat: add password toggle to Register password TextField

--- app/src/main/java/com/example/jetpackcomposetodoapp/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetodoapp/ui/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposetodoapp.ui
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,13 +25,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-
+import com.example.jetpackcomposetodoapp.R
 import com.example.jetpackcomposetodoapp.ui.theme.JetpackComposeToDoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -82,7 +88,20 @@ fun Register(viewModel: RegisterViewModel = hiltViewModel()) {
             value = password,
             onValueChange = { password = it },
             label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                val image = if (isPasswordVisible)
+                    painterResource(R.drawable.ic_visibility)
+                else
+                    painterResource(R.drawable.ic_visibility_off)
+
+                IconButton(onClick = {isPasswordVisible = !isPasswordVisible}) {
+                    Icon(painter = image, contentDescription = "Toggle password visibility")
+                }
+            }
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
