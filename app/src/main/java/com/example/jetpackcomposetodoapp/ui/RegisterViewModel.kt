94c8e60ae90d7621b9973a4b459158a8897ac0a9
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-05 23:10:27 -0300

    feat: add register business logic

--- app/src/main/java/com/example/jetpackcomposetodoapp/ui/RegisterViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposetodoapp/ui/RegisterViewModel.kt
@@ -7,33 +7,64 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpackcomposetodoapp.data.AuthRepository
 import com.example.jetpackcomposetodoapp.di.Register
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuthException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor(
-    @Register private val authRepository: AuthRepository
+    @Register private val authRepository: AuthRepository,
+    private val providePasswordRegex: Regex
 ) : ViewModel() {
 
-    private val _showMessage = MutableLiveData<Boolean>()
-    val showMessage: LiveData<Boolean> get() = _showMessage
+    private val _isRegistrationSuccessful = MutableLiveData<Boolean>()
+    val isRegistrationSuccessful: LiveData<Boolean> get() = _isRegistrationSuccessful
 
     private val _registerToLogin = MutableLiveData<Boolean>()
     val registerToLogin: LiveData<Boolean> get() = _registerToLogin
 
+    private val _toastMessage = MutableLiveData<CharSequence>()
+    val toastMessage: LiveData<CharSequence> get() = _toastMessage
 
     fun registerUser(email: String, password: String) {
-        viewModelScope.launch {
-            val result = authRepository.authenticate(email, password)
-
-            if (result.isSuccess) {
-                _showMessage.value = true
-            } else {
-                Log.e("RegisterViewModel", "Registration failed", result.exceptionOrNull())
-               // TODO: Handle registration failure
-            }
+        if (!providePasswordRegex.matches(password)) {
+            Log.d("RegisterViewModel", "Password Regex NOT matches")
+            return
         }
+
+        viewModelScope.launch {
+            try {
+                Log.d("RegisterViewModel", "Starting authentication")
+                val result = authRepository.authenticate(email, password)
+                Log.d("RegisterViewModel", "Authentication result received: $result")
+
+                if (result.isSuccess) {
+                    _isRegistrationSuccessful.value = true
+                    _toastMessage.value = "Registration successful"
+                    Log.d("RegisterViewModel", "Registration successful")
+                } else {
+                    _isRegistrationSuccessful.value = false
+                    _toastMessage.value = "Registration failed: ${result.exceptionOrNull()?.message}"
+                    Log.d("RegisterViewModel", "Registration failed, result: ${result.exceptionOrNull()?.message}")
+                }
+            } catch (e: FirebaseAuthException) {
+                Log.e("RegisterViewModel", "Firebase authentication error", e)
+                _isRegistrationSuccessful.value = false
+                _toastMessage.value = "Firebase authentication error: ${e.message}"
+            } catch (e: Exception) {
+                Log.e("RegisterViewModel", "Registration failed", e)
+                _isRegistrationSuccessful.value = false
+                _toastMessage.value = "Error: ${e.message}"
+            }
+
+            Log.d("RegisterViewModel", "_isRegisterSuccessful: ${_isRegistrationSuccessful.value}")
+        }
+    }
+
+    fun isPasswordValid(password: String): Boolean {
+        return providePasswordRegex.matches(password)
     }
 
     fun registerToLogin() {
