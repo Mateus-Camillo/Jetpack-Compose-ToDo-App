71b5e2e600186868ebba7fe6fadcd27df56d5f17
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-16 23:29:21 -0300

    feat: change part of register validation logic to firebaseAuthException function

--- app/src/main/java/com/example/jetpackcomposetodoapp/ui/RegisterViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposetodoapp/ui/RegisterViewModel.kt
@@ -5,18 +5,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.jetpackcomposetodoapp.data.AuthExceptionRepository
 import com.example.jetpackcomposetodoapp.data.AuthRepository
 import com.example.jetpackcomposetodoapp.di.Register
-import com.google.firebase.FirebaseException
-import com.google.firebase.auth.FirebaseAuthException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor(
-    @Register private val authRepository: AuthRepository,
-    private val providePasswordRegex: Regex
+    private val providePasswordRegex: Regex,
+    @Register private val authExceptionRepository: AuthExceptionRepository
 ) : ViewModel() {
 
     private val _isRegistrationSuccessful = MutableLiveData<Boolean>()
@@ -35,43 +34,16 @@ class RegisterViewModel @Inject constructor(
         }
 
         viewModelScope.launch {
-            try {
-                Log.d("RegisterViewModel", "Starting authentication")
-                val result = authRepository.authenticate(email, password)
-                Log.d("RegisterViewModel", "Authentication result received: $result")
-
-                if (result.isSuccess) {
-                    _isRegistrationSuccessful.value = true
-                    _toastMessage.value = "Registration successful"
-                    Log.d("RegisterViewModel", "Registration successful")
-                } else {
-                    _isRegistrationSuccessful.value = false
-                    _toastMessage.value = "Registration failed: ${result.exceptionOrNull()?.message}"
-                    Log.d("RegisterViewModel", "Registration failed, result: ${result.exceptionOrNull()?.message}")
-                }
-            } catch (e: FirebaseAuthException) {
-                Log.e("RegisterViewModel", "Firebase authentication error", e)
-                _isRegistrationSuccessful.value = false
-                _toastMessage.value = "Firebase authentication error: ${e.message}"
-            } catch (e: Exception) {
-                Log.e("RegisterViewModel", "Registration failed", e)
-                _isRegistrationSuccessful.value = false
-                _toastMessage.value = "Error: ${e.message}"
-            }
-
-            Log.d("RegisterViewModel", "_isRegisterSuccessful: ${_isRegistrationSuccessful.value}")
+            authExceptionRepository.firebaseAuthException(
+                email,
+                password,
+                _isRegistrationSuccessful,
+                _toastMessage)
         }
+
     }
 
     fun isPasswordValid(password: String): Boolean {
         return providePasswordRegex.matches(password)
-    }
-
-    fun registerToLogin() {
-        _registerToLogin.value = true
-    }
-
-    fun onRegisterToLoginComplete() {
-        _registerToLogin.value = false
     }
 }
