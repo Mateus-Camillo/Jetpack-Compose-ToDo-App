ad3b030acd2498dde4dcb2cad2cb2604c8240f78
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-22 16:41:03 -0300

    feat: implement signOutUser function

--- app/src/main/java/com/example/jetpackcomposetodoapp/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/example/jetpackcomposetodoapp/repository/LoginRepositoryImpl.kt
@@ -1,18 +1,28 @@
 package com.example.jetpackcomposetodoapp.repository
 
-import com.example.jetpackcomposetodoapp.data.AuthRepository
+import com.example.jetpackcomposetodoapp.data.LoginRepository
+import com.example.jetpackcomposetodoapp.data.RegisterRepository
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class LoginRepositoryImpl @Inject constructor(
     private val firebaseAuth: FirebaseAuth
-) : AuthRepository {
+) : LoginRepository {
 
-    override suspend fun authenticate(email: String, password: String): Result<String> {
+    override suspend fun loginUser(email: String, password: String): Result<String> {
         return try {
             val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
             Result.success(result.user?.uid ?: "User is logged but UID is null")
+        } catch(e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun signOutUser(): Result<String> {
+        return try {
+            firebaseAuth.signOut()
+            Result.success("User signed out successfully")
         } catch(e: Exception) {
             Result.failure(e)
         }
