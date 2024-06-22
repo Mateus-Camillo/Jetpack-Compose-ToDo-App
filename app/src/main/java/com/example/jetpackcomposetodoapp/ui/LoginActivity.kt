331aa903e8308e16e0c9dc979994e9a9b15ae3a4
Mateus-Camillo <camillomateus01@gmail.com>
2024-06-22 16:40:04 -0300

    feat: create login ui

--- app/src/main/java/com/example/jetpackcomposetodoapp/ui/LoginActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetodoapp/ui/LoginActivity.kt
@@ -4,19 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.jetpackcomposetodoapp.ui.ui.theme.JetpackComposeToDoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +48,60 @@ class LoginActivity : ComponentActivity() {
 }
 
 @Composable
-fun Login() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+fun Login(viewModel: LoginViewModel = hiltViewModel()) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Hello!",
-            textAlign = TextAlign.Center
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        ClickableText(text = buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Color.Blue)) {
+                append("Forgot Password?")
+            }
+        },
+            onClick = {
+                      // TODO: Implement forgot password OnClick function
+                      print("Forgot Password")
+            },
+            modifier = Modifier.align(Alignment.End)
+            )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                viewModel.loginUser(email, password)
+            },
+
+
+            modifier = Modifier.fillMaxWidth()
+        ) { Text("Login") }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
